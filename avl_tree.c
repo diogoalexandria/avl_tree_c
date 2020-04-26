6849a110041169969a3fd2da273fc2766021c75c
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2020-04-26 16:59:49 -0300

    Função de rotação simples a direita - rotação ll

--- avl_tree.c
+++ avl_tree.c
@@ -4,9 +4,9 @@
 
 typedef struct NODE {
     int info;
-    int alt;
-    struct NODE *left;
-    struct NODE *right;    
+    int height;
+    struct NODE *left_child;
+    struct NODE *right_child;    
 } Node;
 
 avlTree* create_tree() {
@@ -39,8 +39,8 @@ void free_node(Node* node) { // Espera um ponteiro para Node, inicialmente o val
     if (node == NULL)
         return;
     
-    free_node(node->left);
-    free_node(node->right);
+    free_node(node->left_child);
+    free_node(node->right_child);
     free(node);
     node = NULL;
 }
@@ -56,20 +56,59 @@ int tree_height(avlTree* root) { //Recebe o conteúdo do ponteiro, no caso o end
     if (is_empty(root))
         return 0;
 
-    int left_height = tree_height(&((*root)->left)); // Endereço do nó da esquerda do nó root
-    int right_height = tree_height(&((*root)->right)); // tree_height(&((*root)->right));
+    int left_height = tree_height(&((*root)->left_child)); // Endereço do nó da esquerda do nó root
+    int right_height = tree_height(&((*root)->right_child)); // tree_height(&((*root)->right));
     if (left_height > right_height)
         return (left_height + 1);
     else
         return(right_height + 1);
 }
 
+int node_height(Node* node) {
+    if(node == NULL)
+        return  -1;
+    else
+        return node->height;
+}
+
+int balancing_factor(Node* node) {
+    return labs(node_height(node->left_child) - node_height(node->right_child));
+}
+
+int biggest_height(int left, int right) {
+    if (left > right)
+        return left;
+    else
+        return right; 
+}
+
+void ll_rotation(avlTree* root) {    
+    Node* unbalanced_node = root;
+    Node* auxiliar_node;
+
+    auxiliar_node = unbalanced_node->left_child; // Pega o filho à esquerda do ancestral mais próximo desbalanceado (Fator de balanceamento = +2) ao nó que foi inserido a árvore.
+    unbalanced_node->left_child = auxiliar_node->right_child; // O filho à esquerda do nó desbalanceado recebe o valor do filho à direita do nó desbalanceado.
+    auxiliar_node->right_child = unbalanced_node; //O nó que era filho a esquerda do nó desbalanceado, seu filho a direita recebe o nó desbalanceado.
+
+    int new_height;
+    new_height = biggest_height(node_height(unbalanced_node->left_child),node_height(unbalanced_node->right_child)) +1;
+    unbalanced_node->height = new_height; //Atualiza a altura que está o nó inicialmente desbalanceado.
+    new_height = biggest_height(node_height(auxiliar_node->left_child), unbalanced_node->height) + 1;
+    auxiliar_node->height = new_height; //Atualiza a altura do nó que assume a posição
+
+    unbalanced_node = auxiliar_node; // O filho que estava a esquerda do nó desbalanceado assume a posição desse nó
+}
+
+void rr_rotation(){
+
+}
+
 int total_number(avlTree* root) { // Retorna o total de elementos na árvore;
     if (is_empty(root))
         return 0;
     
-    int number_left = total_number(&((*root)->left));
-    int number_right = total_number(&(*root)->right);
+    int number_left = total_number(&((*root)->left_child));
+    int number_right = total_number(&(*root)->right_child);
     int total = number_left + number_right + 1;
     return total;
 }
@@ -79,25 +118,25 @@ void pre_order(avlTree* root) {
         return;
     }    
     printf("%d\n",(*root)->info);
-    pre_order(&((*root)->left));    
-    pre_order(&((*root)->right));
+    pre_order(&((*root)->left_child));    
+    pre_order(&((*root)->right_child));
 }
 
 void in_order(avlTree* root) {
     if (is_empty(root)){        
         return;
     }
-    in_order(&((*root)->left));
+    in_order(&((*root)->left_child));
     printf("%d\n",(*root)->info);
-    in_order(&((*root)->right));
+    in_order(&((*root)->right_child));
 }
 
 void post_order(avlTree* root) {
     if (is_empty(root)) {        
         return;
     }
-    post_order(&((*root)->left));
-    post_order(&((*root)->right));
+    post_order(&((*root)->left_child));
+    post_order(&((*root)->right_child));
     printf("%d\n",(*root)->info);
 }
 
@@ -105,8 +144,8 @@ Node* create_node(int value) {
     Node *new = (Node*) malloc (sizeof(Node));
     if (new != NULL){
         new->info = value;
-        new->right = NULL;
-        new-> left = NULL;                
+        new->right_child = NULL;
+        new-> left_child = NULL;                
     } else {
         printf("Houve um erro na criação do nó.\n");        
     }
@@ -125,9 +164,9 @@ int insert_value(avlTree* root, int value) {
     }
     int answer;
     if ((*root)->info > value) {                
-        answer = insert_value(&((*root)->left), value);
+        answer = insert_value(&((*root)->left_child), value);
     } else if ((*root)->info < value) {              
-        answer = insert_value(&((*root)->right), value);
+        answer = insert_value(&((*root)->right_child), value);
     } else {
         printf("Esse número já existe na árvore.");
         return 0;
@@ -137,22 +176,22 @@ int insert_value(avlTree* root, int value) {
 
 Node* remove_node(Node* this) {
     Node *auxiliar_node, *return_node;
-    if(this->left == NULL) {
-        return_node = this->right;
+    if(this->left_child == NULL) {
+        return_node = this->right_child;
         free(this);
         return return_node;
     }
     auxiliar_node = this;
-    return_node = this->left;
-    while (return_node->right != NULL) {
+    return_node = this->left_child;
+    while (return_node->right_child != NULL) {
         auxiliar_node = return_node;
-        return_node = return_node->right;
+        return_node = return_node->right_child;
     }
     if(auxiliar_node != this) {
-        auxiliar_node->right = return_node->left;
-        return_node->left = this->left;
+        auxiliar_node->right_child = return_node->left_child;
+        return_node->left_child = this->left_child;
     }
-    return_node->right = this->right;
+    return_node->right_child = this->right_child;
     printf("Nó deletado: %d\n", this->info);
     free(this);
     return return_node;    
@@ -169,18 +208,18 @@ int delete_value(avlTree* root, int value) {
             if (this_node == *root) // Testa se está removendo a raiz da árvore
                 *root = remove_node(this_node); // O retorno da função remove_node seŕa nova raiz da ŕavore
             else {
-                if (previous->right == this_node) // Testa se o nó que será exluído está a direita do nó anterior
-                    previous->right = remove_node(this_node); 
+                if (previous->right_child == this_node) // Testa se o nó que será exluído está a direita do nó anterior
+                    previous->right_child = remove_node(this_node); 
                 else                              // Senão está a esquerda
-                    previous->left = remove_node(this_node);                    
+                    previous->left_child = remove_node(this_node);                    
             }
             return 1;
         }
         previous = this_node;
         if ( value > this_node->info)
-            this_node = this_node->right;
+            this_node = this_node->right_child;
         else
-            this_node = this_node->left;
+            this_node = this_node->left_child;
     }
     printf("Elemento não encontrado para exclusão.\n");
     return 0;    
@@ -194,9 +233,9 @@ int find_value(avlTree* root, int value) {
     if((*root)->info == value) {
         return 1;
     } else if ((*root)->info > value) {
-        answer = find_value(&((*root)->left), value);
+        answer = find_value(&((*root)->left_child), value);
     } else {
-        answer = find_value(&((*root)->right), value);
+        answer = find_value(&((*root)->right_child), value);
     }
     return answer;
 }
